6f0dc229dc2cbcf59581af0bca5a4b9f634c5e6f
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-01-02 21:09:58 +0200

    Add files via upload

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApp10
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.colorTab = new System.Windows.Forms.TabPage();
+			this.button2 = new System.Windows.Forms.Button();
+			this.colorLabel = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.button3 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.button4 = new System.Windows.Forms.Button();
+			this.fileLines = new System.Windows.Forms.ListBox();
+			this.myColorDialog = new System.Windows.Forms.ColorDialog();
+			this.myOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.menuStrip1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.colorTab.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.tabPage3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(322, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+			this.exitToolStripMenuItem.Text = "&Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+			this.aboutToolStripMenuItem.Text = "&About";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.colorTab);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 24);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(322, 275);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// colorTab
+			// 
+			this.colorTab.Controls.Add(this.button2);
+			this.colorTab.Controls.Add(this.colorLabel);
+			this.colorTab.Controls.Add(this.label1);
+			this.colorTab.Location = new System.Drawing.Point(4, 22);
+			this.colorTab.Name = "colorTab";
+			this.colorTab.Padding = new System.Windows.Forms.Padding(3);
+			this.colorTab.Size = new System.Drawing.Size(314, 249);
+			this.colorTab.TabIndex = 0;
+			this.colorTab.Text = "Color";
+			this.colorTab.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(176, 183);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(101, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Background Color";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// colorLabel
+			// 
+			this.colorLabel.Location = new System.Drawing.Point(22, 183);
+			this.colorLabel.Name = "colorLabel";
+			this.colorLabel.Size = new System.Drawing.Size(107, 23);
+			this.colorLabel.TabIndex = 1;
+			this.colorLabel.Text = "Foreground Color";
+			this.colorLabel.UseVisualStyleBackColor = true;
+			this.colorLabel.Click += new System.EventHandler(this.colorLabel_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(21, 35);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(256, 55);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "I Love .Net";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.button3);
+			this.tabPage2.Controls.Add(this.pictureBox1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(314, 249);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Image";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(119, 195);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Insert Image";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(308, 171);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.button4);
+			this.tabPage3.Controls.Add(this.fileLines);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(314, 249);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "File";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(100, 193);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(108, 23);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "Insert Text File";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// fileLines
+			// 
+			this.fileLines.Dock = System.Windows.Forms.DockStyle.Top;
+			this.fileLines.FormattingEnabled = true;
+			this.fileLines.Location = new System.Drawing.Point(3, 3);
+			this.fileLines.Name = "fileLines";
+			this.fileLines.Size = new System.Drawing.Size(308, 160);
+			this.fileLines.TabIndex = 0;
+			// 
+			// myOpenFileDialog
+			// 
+			this.myOpenFileDialog.FileName = "openFileDialog1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(322, 299);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.colorTab.ResumeLayout(false);
+			this.colorTab.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.tabPage3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage colorTab;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button colorLabel;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ListBox fileLines;
+		private System.Windows.Forms.ColorDialog myColorDialog;
+		private System.Windows.Forms.OpenFileDialog myOpenFileDialog;
+	}
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Are you sure you want to Exit?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+					   == DialogResult.Yes)
+			{
+				Application.Exit();
+			}
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Version 1.9.0", "File Explorer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			if (myColorDialog.ShowDialog() == DialogResult.OK)
+			{
+				colorLabel.ForeColor = myColorDialog.Color;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (myColorDialog.ShowDialog() == DialogResult.OK)
+			{
+				label1.BackColor = myColorDialog.Color;
+			}
+		}
+
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			myOpenFileDialog.Title = "Choose an Image file";
+			myOpenFileDialog.Filter = "Image files(*.jpg)| *.jpg";
+			if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				pictureBox1.Image = new Bitmap(myOpenFileDialog.FileName);
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			myOpenFileDialog.Title = "Choose a text file";
+			myOpenFileDialog.Filter = "Text files(*.txt)| *.txt";
+			if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				fileLines.Items.Clear();
+
+				// Chinese
+
+				var fileStream = new FileStream(myOpenFileDialog.FileName, FileMode.Open, FileAccess.Read);
+				using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+				{
+					string line;
+					while ((line = streamReader.ReadLine()) != null)
+					{
+						fileLines.Items.Add(line);
+					}
+
+				}
+			}
+		}
+
+		private void colorLabel_Click(object sender, EventArgs e)
+		{
+			if (myColorDialog.ShowDialog() == DialogResult.OK)
+			{
+				label1.ForeColor = myColorDialog.Color;
+			}
+		}
+	}
+}
